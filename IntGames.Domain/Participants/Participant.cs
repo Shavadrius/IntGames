0d09e3d2c3ef46042d89f1817471d4a1a1751fe3
Vadim Shaykhislamov <vshaykhislamov@buyist.com>
2026-03-16 14:50:24 +0500

    add participant entity

---
+++ IntGames.Domain/Participants/Participant.cs
@@ -0,0 +1,52 @@
+﻿using IntGames.Domain.Abstractions;
+using IntGames.Domain.Players;
+using IntGames.Domain.Shared;
+
+namespace IntGames.Domain.Participants;
+
+public sealed class Participant: Entity
+{
+    private Participant(
+        Guid id, 
+        Guid playerId, 
+        Guid tournamentId, 
+        FirstName firstName, 
+        LastName lastName, 
+        Patronymic? patronymic, 
+        DateOnly? birthday, 
+        ParticipationStatus status) : base(id)
+    {
+        PlayerId = playerId;
+        TournamentId = tournamentId;
+        FirstName = firstName;
+        LastName = lastName;
+        Patronymic = patronymic;
+        Birthday = birthday;
+        Status = status;
+    }
+
+    public Guid PlayerId { get; private init; }
+    public Guid TournamentId { get; private init; }
+
+    //Snapshot of Player info
+    public FirstName FirstName { get; private set; }
+    public LastName LastName { get; private set; }
+    public Patronymic? Patronymic { get; private set; }
+    public DateOnly? Birthday { get; private set; }
+
+    //Specific fields
+    public ParticipationStatus Status { get; private set; }
+
+    public static Result<Participant> FromPlayer(Player player, Guid tournamentId)
+    {
+        return new Participant(Guid.NewGuid(),
+            tournamentId,
+            player.Id,
+            player.FirstName,
+            player.LastName,
+            player.Patronymic,
+            player.Birthday,
+            ParticipationStatus.PendingApproval);
+    }
+
+}
---
+++ IntGames.Domain/Participants/ParticipationStatus.cs
@@ -0,0 +1,24 @@
+﻿namespace IntGames.Domain.Participants;
+
+public enum ParticipationStatus
+{
+    /// <summary>
+    /// Participant has submitted a registration request, awaiting approval
+    /// </summary>
+    PendingApproval = 0,
+
+    /// <summary>
+    /// Registration has been approved (and payment received if required)
+    /// </summary>
+    Approved = 1,
+
+    /// <summary>
+    /// Registration has been rejected by the organizer
+    /// </summary>
+    Rejected = 2,
+
+    /// <summary>
+    /// Registration has been approved, awaiting payment (for paid tournaments)
+    /// </summary>
+    AwaitingPayment = 3,
+}
